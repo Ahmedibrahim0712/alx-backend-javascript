afb56b6b9e05530f2fceb87b549fdb5a51c0a3cb
Ahmedibrahim0712 <144346741+Ahmedibrahim0712@users.noreply.github.com>
2024-10-25 20:34:54 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
-process.stdin.setEncoding('utf-8');
-
-process.stdout.write('Welcome to Holberton School, what is your name? \n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name.toString()}\n`);
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing \n');
+  process.stdout.write('This important software is now closing\n');
 });
